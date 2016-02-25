779004bb4676fb5fbc087e088becf1c222d3ccb6
MohsinKhan <mohsin.khan@immediate.co.uk>
2016-02-25 00:12:16 +0000

    Completed Story 2 with test cases and refactoring

---
+++ ContentConsole/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using ContentConsole.Models;
+
+namespace ContentConsole.Repositories
+{
+    public interface IRepository
+    {
+        int Add(BadWord word);
+        int Remove(BadWord word);
+        IEnumerable<BadWord> GetList();
+    }
+}
